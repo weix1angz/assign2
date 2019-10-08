47255ffb7e9c8e61f3af5cd372c24e71bf5e5e4c
Weixiang Zhang <weixiangz@weixiangdembp.3520.dhcp.asu.edu>
2019-10-08 14:17:25 -0700

    initial version

---
+++ cse360assign2/AddingMachine.java
@@ -0,0 +1,56 @@
+/**
+ * @author Weixiang Zhang
+ * Course: CSE360
+ * Assignment: 2
+ * Description: This file contains a class with several methods simulating a adding machine.
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the total value.
+	 * @return the total value 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method add a value to total.
+	 * @param value An integer to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method subtract a value to total.
+	 * @param value An integer to be subtracted from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * @return A string indicating the computing process
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * clear the string and total
+	 */
+	public void clear() {
+	
+	}
+}
