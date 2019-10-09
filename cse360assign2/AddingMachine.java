f6150865e32811340a878926b0005eaab33c2351
Weixiang Zhang <weixiangz@WeixiangdeMacBook-Pro.local>
2019-10-08 17:11:38 -0700

    second version

--- cse360assign2/AddingMachine.java
+++ cse360assign2/AddingMachine.java
@@ -9,48 +9,59 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	
+	private String history;
 	/**
 	 * Constructor
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	/**
+	 * Get the total value.
 	 * This method returns the total value.
 	 * @return the total value 
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
+	 * Add to total
 	 * This method add a value to total.
 	 * @param value An integer to be added to the total
 	 */
 	public void add (int value) {
+		total += value;
+		history += " + " + value;
 		
 	}
 	
 	/**
+	 * Subtract from total
 	 * This method subtract a value to total.
 	 * @param value An integer to be subtracted from the total
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	/**
+	 * Return this history of transactions
+	 * This method returns a string that indicating the computing process
 	 * @return A string indicating the computing process
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	/**
-	 * clear the string and total
+	 * Reset the history and total
+	 * This method reset the total and history as same as their initial value.
 	 */
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
